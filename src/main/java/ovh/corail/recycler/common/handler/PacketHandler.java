b4d73d063841e98eed4f09b7c2091c4b35bb53e8
Corail31 <m.pietquin@free.fr>
2016-03-20 00:52:25 +0100

    Update PacketHandler.java

--- src/main/java/ovh/corail/recycler/common/handler/PacketHandler.java
+++ src/main/java/ovh/corail/recycler/common/handler/PacketHandler.java
@@ -14,27 +14,5 @@ public class PacketHandler {
 		int id = 0;
 		INSTANCE.registerMessage(ButtonMessage.class, ButtonMessage.class, id++, Side.SERVER);
 		INSTANCE.registerMessage(VisualMessage.class, VisualMessage.class, id++, Side.SERVER);
-		/*
-		 * INSTANCE.registerMessage(StacksMessage.class, StacksMessage.class,
-		 * id++, Side.CLIENT); INSTANCE.registerMessage(RequestMessage.class,
-		 * RequestMessage.class, id++, Side.SERVER);
-		 * INSTANCE.registerMessage(ClearMessage.class, ClearMessage.class,
-		 * id++, Side.SERVER); INSTANCE.registerMessage(SortMessage.class,
-		 * SortMessage.class, id++, Side.SERVER);
-		 * INSTANCE.registerMessage(SyncMessage.class, SyncMessage.class, id++,
-		 * Side.CLIENT); INSTANCE.registerMessage(RecipeMessage.class,
-		 * RecipeMessage.class, id++, Side.SERVER);
-		 * INSTANCE.registerMessage(LimitMessage.class, LimitMessage.class,
-		 * id++, Side.SERVER); INSTANCE.registerMessage(RemoteMessage.class,
-		 * RemoteMessage.class, id++, Side.SERVER);
-		 * INSTANCE.registerMessage(InsertMessage.class, InsertMessage.class,
-		 * id++, Side.SERVER); INSTANCE.registerMessage(StackMessage.class,
-		 * StackMessage.class, id++, Side.CLIENT);
-		 * INSTANCE.registerMessage(FilterMessage.class, FilterMessage.class,
-		 * id++, Side.SERVER); INSTANCE.registerMessage(TemplateMessage.class,
-		 * TemplateMessage.class, id++, Side.SERVER);
-		 * INSTANCE.registerMessage(FaceMessage.class, FaceMessage.class, id++,
-		 * Side.SERVER);
-		 */
 	}
 }
