aaa09b660eca72684cab94f9fa95e7c6300bf9c3
Corail31 <m.pietquin@free.fr>
2016-03-24 00:57:51 +0100

    Update CommonProxy.java

--- src/main/java/ovh/corail/recycler/common/CommonProxy.java
+++ src/main/java/ovh/corail/recycler/common/CommonProxy.java
@@ -1,17 +1,30 @@
 package ovh.corail.recycler.common;
 
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import ovh.corail.recycler.common.blocks.RecyclerBlock;
+import ovh.corail.recycler.common.handler.ConfigurationHandler;
 import ovh.corail.recycler.common.handler.GuiHandler;
 import ovh.corail.recycler.common.handler.PacketHandler;
+import ovh.corail.recycler.common.items.BasicItem;
+import ovh.corail.recycler.common.items.Disk;
 import ovh.corail.recycler.common.tileentity.RecyclerTile;
 
 public class CommonProxy {
 	public void preInit(FMLPreInitializationEvent event) {
 		GameRegistry.registerTileEntity(RecyclerTile.class, "inventory");
+		ConfigurationHandler.config = new Configuration(event.getSuggestedConfigurationFile());
+		ConfigurationHandler.config.load();
+		ConfigurationHandler.refreshConfig();	
+		Main.items.add(new BasicItem("iron_nugget"));
+		Main.items.add(new BasicItem("diamond_nugget"));
+		Main.items.add(new Disk()); 
+		Main.blocks.add(new RecyclerBlock());
+		MainUtil.getNewRecipes(); 
 		PacketHandler.init();
 	}
 	public void init(FMLInitializationEvent event) {
