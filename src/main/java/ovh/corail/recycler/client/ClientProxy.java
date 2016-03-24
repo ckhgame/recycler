08c44c5d55239086afa99f098becee1f53ee369c
Corail31 <m.pietquin@free.fr>
2016-03-24 01:46:45 +0100

    Update ClientProxy.java

--- src/main/java/ovh/corail/recycler/client/ClientProxy.java
+++ src/main/java/ovh/corail/recycler/client/ClientProxy.java
@@ -8,21 +8,26 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import ovh.corail.recycler.common.CommonProxy;
 import ovh.corail.recycler.common.Main;
+import ovh.corail.recycler.common.MainUtil;
 
 public class ClientProxy extends CommonProxy {
 
 	public void  renderBlocks() {
-		for (int i = 0; i < Main.blocks.size(); i++) {
-			renderItem(Item.getItemFromBlock(Main.blocks.get(i)));
-		}
+		//for (int i = 0; i < Main.blocks.size(); i++) {
+		//	renderItem(Item.getItemFromBlock(Main.blocks.get(i)));
+		//}
+		renderItem(Item.getItemFromBlock(MainUtil.recycler));
 	}
 	public void renderItem(Item item) {
 		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(Main.MODID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
 	}
 	public void  renderItems() {
-		for (int i = 0; i < Main.items.size(); i++) {
-			renderItem(Main.items.get(i));
-		}
+		//for (int i = 0; i < Main.items.size(); i++) {
+		//	renderItem(Main.items.get(i));
+		//}
+		renderItem(MainUtil.iron_nugget);
+		renderItem(MainUtil.diamond_nugget);
+		renderItem(MainUtil.diamond_disk);
 	}
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
