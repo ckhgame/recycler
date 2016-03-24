e1846a257d64e2cb55e0c421bbb9d0f5440fa20a
Corail31 <m.pietquin@free.fr>
2016-03-24 12:27:34 +0100

    Update MainUtil.java

--- src/main/java/ovh/corail/recycler/common/MainUtil.java
+++ src/main/java/ovh/corail/recycler/common/MainUtil.java
@@ -14,6 +14,7 @@ import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import ovh.corail.recycler.common.blocks.RecyclerBlock;
+import ovh.corail.recycler.common.handler.ConfigurationHandler;
 import ovh.corail.recycler.common.items.BasicItem;
 import ovh.corail.recycler.common.items.Disk;
 
@@ -82,6 +83,7 @@ public class MainUtil {
 		GameRegistry.addRecipe(new ItemStack(diamond_nugget, 9),
 				new Object[] { "0", Character.valueOf('0'), new ItemStack(Items.diamond, 1), });
 		/* Podzol (2 terres 2 sables en croix) */
+		if (ConfigurationHandler.craftPodzol) {
 		GameRegistry.addRecipe(new ItemStack(Blocks.dirt, 1, 2), new Object[] { "01", "10", Character.valueOf('0'),
 				new ItemStack(Blocks.dirt, 1, 0), Character.valueOf('1'), new ItemStack(Blocks.sand, 1, 0), });
 		GameRegistry.addRecipe(new ItemStack(Blocks.dirt, 1, 2), new Object[] { "01", "10", Character.valueOf('1'),
@@ -90,7 +92,9 @@ public class MainUtil {
 				new ItemStack(Blocks.dirt, 1, 0), Character.valueOf('1'), new ItemStack(Blocks.sand, 1, 1), });
 		GameRegistry.addRecipe(new ItemStack(Blocks.dirt, 1, 2), new Object[] { "01", "10", Character.valueOf('1'),
 				new ItemStack(Blocks.dirt, 1, 0), Character.valueOf('0'), new ItemStack(Blocks.sand, 1, 1), });
+		}
 		/* Argile (2 granits 2 sables en croix) */
+		if (ConfigurationHandler.craftClay) {
 		GameRegistry.addRecipe(new ItemStack(Blocks.clay, 1, 0), new Object[] { "01", "10", Character.valueOf('0'),
 				new ItemStack(Blocks.stone, 1, 1), Character.valueOf('1'), new ItemStack(Blocks.sand, 1, 0), });
 		GameRegistry.addRecipe(new ItemStack(Blocks.clay, 1, 0), new Object[] { "01", "10", Character.valueOf('1'),
@@ -99,10 +103,14 @@ public class MainUtil {
 				new ItemStack(Blocks.stone, 1, 1), Character.valueOf('1'), new ItemStack(Blocks.sand, 1, 1), });
 		GameRegistry.addRecipe(new ItemStack(Blocks.clay, 1, 0), new Object[] { "01", "10", Character.valueOf('1'),
 				new ItemStack(Blocks.stone, 1, 1), Character.valueOf('0'), new ItemStack(Blocks.sand, 1, 1), });
+		}
 		/* Toile (5 ficelles en croix) */
+		if (ConfigurationHandler.craftWeb) {
 		GameRegistry.addRecipe(new ItemStack(Blocks.web, 1, 0),
 				new Object[] { "0 0", " 0 ", "0 0", Character.valueOf('0'), new ItemStack(Items.string, 1, 0), });
+		}
 		/* Cottes de mailles (lingot de fer et barreaux de fer) */
+		if (ConfigurationHandler.craftChainmail) {
 		GameRegistry.addRecipe(new ItemStack(Items.chainmail_helmet, 1, 0),
 				new Object[] { "000", "1 1", Character.valueOf('0'), new ItemStack(Items.iron_ingot, 1, 0),
 						Character.valueOf('1'), new ItemStack(Blocks.iron_bars, 1, 0), });
@@ -119,6 +127,7 @@ public class MainUtil {
 		GameRegistry.addRecipe(new ItemStack(Items.chainmail_boots, 1, 0),
 				new Object[] { "1 1", "0 0", Character.valueOf('0'), new ItemStack(Items.iron_ingot, 1, 0),
 						Character.valueOf('1'), new ItemStack(Blocks.iron_bars, 1, 0), });
+		}
 		/* Recette du Recycleur */
 		GameRegistry.addRecipe(new ItemStack(recycler, 1), new Object[] { "000", "111", "000", Character.valueOf('0'),
 				new ItemStack(Blocks.cobblestone, 1), Character.valueOf('1'), new ItemStack(Items.iron_ingot, 1), });
