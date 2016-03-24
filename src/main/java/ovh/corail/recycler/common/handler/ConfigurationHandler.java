7c95cefb208c6e34f5cf02127990e10283ee6fd3
Corail31 <m.pietquin@free.fr>
2016-03-24 12:26:42 +0100

    Update ConfigurationHandler.java

--- src/main/java/ovh/corail/recycler/common/handler/ConfigurationHandler.java
+++ src/main/java/ovh/corail/recycler/common/handler/ConfigurationHandler.java
@@ -1,14 +1,19 @@
 package ovh.corail.recycler.common.handler;
 
 import net.minecraftforge.common.config.Configuration;
+import ovh.corail.recycler.common.MainUtil;
 
 public class ConfigurationHandler {
 	public static Configuration config;
 
-	public static boolean recyclerRecycled;
+	public static boolean recyclerRecycled, craftPodzol, craftClay, craftWeb, craftChainmail;
 
 	public static void refreshConfig() {
-		recyclerRecycled=config.getBoolean("recyclerRecycled", config.CATEGORY_GENERAL, false, "Recycler le recycleur ");
+		recyclerRecycled=config.getBoolean("recyclerRecycled", config.CATEGORY_GENERAL, false, MainUtil.getTranslation("config.recyclerRecycled"));
+		craftPodzol=config.getBoolean("craftPodzol", config.CATEGORY_GENERAL, true, MainUtil.getTranslation("config.craftPodzol"));
+		craftClay=config.getBoolean("craftClay", config.CATEGORY_GENERAL, true, MainUtil.getTranslation("config.craftClay"));
+		craftWeb=config.getBoolean("craftWeb", config.CATEGORY_GENERAL, true, MainUtil.getTranslation("config.craftWeb"));
+		craftChainmail=config.getBoolean("craftChainmail", config.CATEGORY_GENERAL, true, MainUtil.getTranslation("config.craftChainmail"));
 		if (config.hasChanged()) {
 			config.save();
 		}
