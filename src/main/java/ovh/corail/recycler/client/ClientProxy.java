d8417f3714a2816cc395aacc1b5bb261226eb709
Corail31 <m.pietquin@free.fr>
2016-03-24 02:04:04 +0100

    Update ClientProxy.java

--- src/main/java/ovh/corail/recycler/client/ClientProxy.java
+++ src/main/java/ovh/corail/recycler/client/ClientProxy.java
@@ -11,34 +11,17 @@ import ovh.corail.recycler.common.Main;
 import ovh.corail.recycler.common.MainUtil;
 
 public class ClientProxy extends CommonProxy {
-
-	public void  renderBlocks() {
-		//for (int i = 0; i < Main.blocks.size(); i++) {
-		//	renderItem(Item.getItemFromBlock(Main.blocks.get(i)));
-		//}
-		renderItem(Item.getItemFromBlock(MainUtil.recycler));
-	}
-	public void renderItem(Item item) {
-		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(Main.MODID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
-	}
-	public void  renderItems() {
-		//for (int i = 0; i < Main.items.size(); i++) {
-		//	renderItem(Main.items.get(i));
-		//}
-		renderItem(MainUtil.iron_nugget);
-		renderItem(MainUtil.diamond_nugget);
-		renderItem(MainUtil.diamond_disk);
-	}
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
 		super.preInit(event);
 	}
+
 	@Override
 	public void init(FMLInitializationEvent event) {
 		super.init(event);
-		renderItems();
-		renderBlocks();
+		MainUtil.render();
 	}
+
 	@Override
 	public void postInit(FMLPostInitializationEvent event) {
 		super.postInit(event);
