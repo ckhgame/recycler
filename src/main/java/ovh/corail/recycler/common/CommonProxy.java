559f3a0139bcafe5f262817ea23296ba7ab054fb
Corail31 <m.pietquin@free.fr>
2016-03-25 04:47:30 +0100

    Update CommonProxy.java
    Json Recipes

--- src/main/java/ovh/corail/recycler/common/CommonProxy.java
+++ src/main/java/ovh/corail/recycler/common/CommonProxy.java
@@ -1,5 +1,23 @@
 package ovh.corail.recycler.common;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.JsonParser;
+
+import jdk.nashorn.internal.parser.JSONParser;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -15,17 +33,39 @@ import ovh.corail.recycler.common.items.Disk;
 import ovh.corail.recycler.common.tileentity.RecyclerTile;
 
 public class CommonProxy {
-	public void preInit(FMLPreInitializationEvent event) {
+	public void preInit(FMLPreInitializationEvent event) throws IOException {
 		GameRegistry.registerTileEntity(RecyclerTile.class, "inventory");
 		ConfigurationHandler.config = new Configuration(event.getSuggestedConfigurationFile());
 		ConfigurationHandler.config.load();
 		ConfigurationHandler.refreshConfig();
-		MainUtil.init();  
+		MainUtil.init();
 		PacketHandler.init();
+		File questFile = new File(event.getModConfigurationDirectory(), "recyclingRecipes.json");
+		if (!questFile.exists()) {
+			questFile.createNewFile();
+			FileWriter fw = new FileWriter(questFile);
+			List<jsonRecipe> jsonRecipesList = new ArrayList<jsonRecipe>();
+			//jsonRecipesList
+			//		.add(new jsonRecipe("minecraft:iron_ingot:1:0", new String[] { "minecraft:diamond:1:0" }));
+			fw.write(new GsonBuilder().setPrettyPrinting().create().toJson(jsonRecipesList));
+			fw.close();
+		}
+		// TODO
+		List<jsonRecipe> recipes = new Gson().fromJson(new BufferedReader(new FileReader(questFile)),
+				new TypeToken<List<jsonRecipe>>() {}.getType());
+		Recipe recipe = null;
+		for (int i = 0; i < recipes.size(); i++) {
+			jsonRecipe currentRecipe = recipes.get(i);
+			recipe = MainUtil.convertJsonRecipe(currentRecipe);
+			RecyclingManager.getInstance().addRecipe(recipe);
+		}
+
 	}
+
 	public void init(FMLInitializationEvent event) {
 		NetworkRegistry.INSTANCE.registerGuiHandler(Main.instance, new GuiHandler());
 	}
+
 	public void postInit(FMLPostInitializationEvent event) {
 	}
 }
