f16094db256c82415467c043949310e6e080ca7e
Corail31 <m.pietquin@free.fr>
2016-03-26 02:24:36 +0100

    Update CommonProxy.java

--- src/main/java/ovh/corail/recycler/common/CommonProxy.java
+++ src/main/java/ovh/corail/recycler/common/CommonProxy.java
@@ -5,32 +5,26 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import com.google.gson.JsonParser;
 
-import jdk.nashorn.internal.parser.JSONParser;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import ovh.corail.recycler.common.blocks.RecyclerBlock;
 import ovh.corail.recycler.common.handler.ConfigurationHandler;
 import ovh.corail.recycler.common.handler.GuiHandler;
 import ovh.corail.recycler.common.handler.PacketHandler;
-import ovh.corail.recycler.common.items.BasicItem;
-import ovh.corail.recycler.common.items.Disk;
 import ovh.corail.recycler.common.tileentity.RecyclerTile;
+
+import com.google.gson.JsonParser;
+
+
 
 public class CommonProxy {
 	public void preInit(FMLPreInitializationEvent event) throws IOException {
