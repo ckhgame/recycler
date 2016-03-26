e13f564ac985e7ba79bb13b3a223c6dae6e615cd
Corail31 <m.pietquin@free.fr>
2016-03-26 02:26:31 +0100

    Update ClientProxy.java

--- src/main/java/ovh/corail/recycler/client/ClientProxy.java
+++ src/main/java/ovh/corail/recycler/client/ClientProxy.java
@@ -2,14 +2,10 @@ package ovh.corail.recycler.client;
 
 import java.io.IOException;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import ovh.corail.recycler.common.CommonProxy;
-import ovh.corail.recycler.common.Main;
 import ovh.corail.recycler.common.MainUtil;
 
 public class ClientProxy extends CommonProxy {
