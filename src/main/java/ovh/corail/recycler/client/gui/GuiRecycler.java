705e1a01e41304768487618bc4331c3f71bfdd2f
Corail31 <m.pietquin@free.fr>
2016-03-28 23:35:26 +0200

    Auto recycle

--- src/main/java/ovh/corail/recycler/client/gui/GuiRecycler.java
+++ src/main/java/ovh/corail/recycler/client/gui/GuiRecycler.java
@@ -5,17 +5,20 @@ import java.io.IOException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.NetworkRegistry.TargetPoint;
 import ovh.corail.recycler.common.ContainerRecycler;
 import ovh.corail.recycler.common.Main;
 import ovh.corail.recycler.common.MainUtil;
 import ovh.corail.recycler.common.RecyclingManager;
 import ovh.corail.recycler.common.handler.PacketHandler;
 import ovh.corail.recycler.common.packets.ButtonMessage;
+import ovh.corail.recycler.common.packets.ProgressMessage;
 import ovh.corail.recycler.common.tileentity.RecyclerTile;
 
 public class GuiRecycler extends GuiContainer {
@@ -24,7 +27,8 @@ public class GuiRecycler extends GuiContainer {
 	public int k = 0;
 	public RecyclerTile inventory;
 	private static ResourceLocation texture = new ResourceLocation(Main.MODID + ":textures/recycler.png");
-
+	private static ResourceLocation texture2 = new ResourceLocation(Main.MODID + ":textures/items/recycler.png");
+	
 	public GuiRecycler(EntityPlayer player, World world, int x, int y, int z, RecyclerTile inventory) {
 		super(new ContainerRecycler(player, world, x, y, z, inventory));
 		this.inventory = inventory;
@@ -37,18 +41,12 @@ public class GuiRecycler extends GuiContainer {
 
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-		int posX = (this.width) / 2;
-		int posY = (this.height) / 2;
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		mc.renderEngine.bindTexture(texture);
 		int k = (this.width - this.xSize) / 2;
 		int l = (this.height - this.ySize) / 2;
 		this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
-		zLevel = 100.0F;
-	}
-
-	public void updateScreen() {
-		super.updateScreen();
+		zLevel = 100.0F;	
 	}
 
 	protected void keyTyped(char par1, int par2) {
@@ -62,15 +60,23 @@ public class GuiRecycler extends GuiContainer {
 	}
 
 	protected void drawGuiContainerForegroundLayer(int par1, int par2) {
-		int posX = (this.width) / 2;
-		int posY = (this.height) / 2;
+		int guiLeft = (this.width) / 2;
+		int guiTop = (this.height) / 2;
 		if (inventory.getStackInSlot(0)!=null) {
 			RecyclingManager rm = RecyclingManager.getInstance();
 			int num_recipe=rm.hasRecipe(inventory.getStackInSlot(0));
 			if (num_recipe>0) {
 				int inputCount=rm.getRecipe(num_recipe).getItemRecipe().stackSize;
-				this.fontRendererObj.drawString("X "+Integer.toString(inputCount), (32), (17), 0xffffff);
-			}		
+				this.fontRendererObj.drawString("X "+Integer.toString(inputCount), (32), (14), 0xffffff);
+			}
+			// TODO Current Changes
+			if (inventory.isWorking()) {
+				mc.renderEngine.bindTexture(texture2);
+				drawTexturedModalRect(90, 19, 100, 104, 22, 12);
+				int widthWorking=(int) Math.floor((double) inventory.getPercentWorking()*20.0/100);
+				drawTexturedModalRect(91, 20, 40, 140, widthWorking, 10);
+				this.fontRendererObj.drawString(Integer.toString(inventory.getPercentWorking())+" %", (93), (22), 0xffffff);
+			}
 		}
 	}
 
@@ -85,9 +91,9 @@ public class GuiRecycler extends GuiContainer {
 		this.guiTop = (this.height - 166) / 2;
 		Keyboard.enableRepeatEvents(true);
 		this.buttonList.clear();
-		int posX = (this.width) / 2;
-		int posY = (this.height) / 2;
-		this.buttonList.add(new GuiButton(0, this.guiLeft + 50, this.guiTop + 12, 54, 16, MainUtil.getTranslation("button.recycle")));
+		// TODO Current Changes
+		this.buttonList.add(new GuiButton(0, this.guiLeft + 40, this.guiTop + 12, 54, 16, MainUtil.getTranslation("button.recycle")));
+		this.buttonList.add(new GuiButton(1, this.guiLeft + 95, this.guiTop + 5, 16, 16, "Auto"));
 	}
 
 	@Override
@@ -103,6 +109,9 @@ public class GuiRecycler extends GuiContainer {
 		switch (button.id) {
 		case 0: // Recycle
 			inventory.recycle();
+			break;
+		case 1: // Auto-recycle
+			inventory.switchWorking();
 			break;
 		}
 	}
