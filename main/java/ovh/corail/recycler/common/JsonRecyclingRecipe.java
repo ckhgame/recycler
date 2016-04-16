ff593b4c97bee07e2ba015abe42edd8f8eb14012
Corail31 <m.pietquin@free.fr>
2016-04-16 05:54:21 +0200

    refresh

---
+++ main/java/ovh/corail/recycler/common/JsonRecyclingRecipe.java
@@ -0,0 +1,15 @@
+package ovh.corail.recycler.common;
+
+public class JsonRecyclingRecipe {
+	String inputItem;
+	String[] outputItems;
+	boolean canBeRepaired;
+	public JsonRecyclingRecipe(String inputItem, String[] outputItems, boolean canBeRepaired) {
+		this.inputItem=inputItem;
+		this.outputItems=outputItems;
+		this.canBeRepaired=canBeRepaired;
+	}
+	public JsonRecyclingRecipe(String inputItem, String[] outputItems) {
+		this(inputItem, outputItems, false);
+	}
+}
