59c261752c4e05b1e7a8af7682f8ec791b1a04bb
Corail31 <m.pietquin@free.fr>
2016-05-18 19:32:50 +0200

    version

--- main/java/ovh/corail/recycler/core/Main.java
+++ main/java/ovh/corail/recycler/core/Main.java
@@ -23,7 +23,7 @@ import ovh.corail.recycler.item.ItemIronNugget;
 public class Main {
 	public static final String MOD_ID = "recycler";
 	public static final String MOD_NAME = "Corail Recycler";
-	public static final String MOD_VER = "1.1";
+	public static final String MOD_VER = "1.2";
 	@Instance(Main.MOD_ID)
 	public static Main instance;
 	@SidedProxy(clientSide = "ovh.corail.recycler.core.ClientProxy", serverSide = "ovh.corail.recycler.core.CommonProxy")
